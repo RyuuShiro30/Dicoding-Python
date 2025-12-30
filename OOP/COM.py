4107e8add0e15e2296f6197692c7d88624c516db
RyuuShiro30 <annoraegaprasetyo@gmail.com>
2025-12-30 16:19:38 +0700

    OOP Python Done

--- OOP/COM.py
+++ OOP/COM.py
@@ -21,3 +21,87 @@ print(mobil1.warna)
 
 mobil2 = Mobil()
 print(mobil2.warna)
+
+#Class Constructor
+class Mobil:
+    # Atribut Instance
+    # Penjelasan INIT: "__init__" sebagai fungsi khusus untuk menjadi constructor.
+    # Penjelasan SELF: menggunakan parameter self, yakni sebuah kata kunci 
+    # untuk merujuk pada objek yang sedang diproses saat ini.
+    def __init__(self):
+        self.warna = 'Merah'
+        
+mobil_1 = Mobil()
+mobil_2 = Mobil()
+
+print(mobil_1.warna)
+print(mobil_2.warna)
+
+# Mengubah atribut instance
+mobil_1.warna = "Hitam"
+
+# Menampilkan kembali atribut warna
+print(mobil_1.warna)
+print(mobil_2.warna)
+
+#Method
+def my_decorator(func):
+    def wrapper():
+        print("Sebelum fungsi dieksekusi.")
+        func()
+        print("Setelah fungsi dieksekusi.")
+    return wrapper
+
+# Dekorasi fungsi dengan decorator
+@my_decorator
+def say_hello():
+    print("Hello, world!")
+
+# Memanggil fungsi yang sudah didekorasi
+say_hello()
+
+#Metode dari Objek (Object Method)
+class Mobil:
+    def __init__(self, warna, merek, kecepatan):
+        self.warna = warna
+        self.merek = merek
+        self.kecepatan = kecepatan
+
+    def tambah_kecepatan(self):
+        self.kecepatan += 10
+
+mobil_1 = Mobil("Merah", "DicodingCar", 160)
+print("Sebelum ditambahkan: ")
+print(mobil_1.kecepatan)
+
+mobil_1.tambah_kecepatan()        # Memanggil metode tambah_kecepatan.
+
+print("Setelah ditambahkan: ")
+print(mobil_1.kecepatan)
+
+#Metode secara Statis (Static Method)
+class Mobil:
+    def __init__(self, merek):
+        self.merek = merek
+    
+    @staticmethod
+    def intro_mobil():
+        print("Ini adalah metode dari kelas Mobil")
+        
+Mobil.intro_mobil()
+mobil_1 = Mobil("DicodingCar")
+mobil_1.intro_mobil()
+
+#Metode dari Kelas (Class Method)
+class Mobil:
+    def __init__(self, merek):
+        self.merek = merek
+
+    @classmethod
+    def intro_mobil(cls):
+        print("Ini adalah metode dari kelas Mobil")
+        
+Mobil.intro_mobil()
+mobil_1 = Mobil("DicodingCar")
+mobil_1.intro_mobil()
+
---
+++ OOP/OOP-Tugas.py
@@ -0,0 +1,18 @@
+class Animal:
+    
+    def __init__(self, name: str, age: int, species: str):
+        self.name = name
+        self.age = age
+        self.species = species
+
+class Cat(Animal):
+    def deskripsi(self):
+        return f"{self.name} adalah kucing berjenis {self.species} yang sudah berumur {self.age} tahun"
+    
+    def suara(self):
+        return "Meow!"
+    
+myCat = Cat("Neko", 3, "Persian")
+
+print(myCat.deskripsi())
+print(myCat.suara())
